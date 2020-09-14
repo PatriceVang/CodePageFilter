49154fd5262522215feaf36cc01e98a7566a8c2c
Patrice <patrice@vinova.com.sg>
2020-09-14 13:58:13 +0700

    Add DetailVC

---
+++ UseGit/UseGit/ViewControllẻ/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  UseGit
+//
+//  Created by Apple on 9/14/20.
+//  Copyright © 2020 vinova. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
