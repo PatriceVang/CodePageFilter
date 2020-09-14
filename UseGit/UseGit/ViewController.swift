cb4e2a53ec197a362276f00a958ebd96c9f3d1a6
Patrice <patrice@vinova.com.sg>
2020-09-14 13:39:58 +0700

    First commit

--- UseGit/UseGit/ViewController.swift
+++ UseGit/UseGit/ViewController.swift
@@ -13,8 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+        
+        
+        
+        
     }
-
+    private func setupUI() {
+        
+    }
 
 }
 
